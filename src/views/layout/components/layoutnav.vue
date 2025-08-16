ae0e1165ddd009a0ad3e14c3f89fda94d2bcf579
un0rder <3219430136@qq.com>
2025-08-16 11:19:02 +0800

    token实现nav多模板

--- src/views/layout/components/layoutnav.vue
+++ src/views/layout/components/layoutnav.vue
@@ -1,13 +1,17 @@
 <script setup>
+import { useUserStore } from '@/stores/user';
+import { storeToRefs } from 'pinia';
 
+const { userInfo } = storeToRefs(useUserStore())
+console.log(userInfo)
 </script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userInfo.account }}</a></li>
           <li>
             <!-- el-popconfirm -->
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
